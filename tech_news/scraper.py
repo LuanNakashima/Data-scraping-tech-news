14f5717ee16d38b43255309ab4e7634a48edfd88
LuanMigNak <luan.nakashima@gmail.com>
2022-11-08 17:50:40 -0300

    req 1 e 2

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,12 +1,32 @@
+import requests
+import time
+from parsel import Selector
+from rich import print as rprint
+
 # Requisito 1
 def fetch(url):
+    try:
+        time.sleep(1)
+        response = requests.get(url, headers={ "user-agent": "Fake user-agent" }, timeout=3)
+        if response.status_code == 200:
+            # return response.headers["Content-Type"]
+            return response.text
+        return None
+    except:
+        return None
     """Seu código deve vir aqui"""
 
+
+# rprint(fetch("https://blog.betrybe.com/"))
 
 # Requisito 2
 def scrape_novidades(html_content):
-    """Seu código deve vir aqui"""
+        selector = Selector(html_content)
+        return selector.css(".entry-title a::attr(href)").getall()
+        """Seu código deve vir aqui"""
 
+
+# rprint(fetch("https://blog.betrybe.com/"))
 
 # Requisito 3
 def scrape_next_page_link(html_content):
