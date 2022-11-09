515f1b9679c35df2b42e1dd6f4e306a232a921da
LuanMigNak <luan.nakashima@gmail.com>
2022-11-09 13:15:24 -0300

    req 4 lint de novo grr

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -4,16 +4,17 @@ from parsel import Selector
 from bs4 import BeautifulSoup
 # from rich import print as rprint
 
+
 # Requisito 1
 def fetch(url):
     try:
         time.sleep(1)
-        response = requests.get(url,
-        headers={"user-agent": "Fake user-agent"}, timeout=3)
-        if response.status_code == 200:
-            return response.text
-        raise ValueError('Erro')
-    except:
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+            )
+        response.raise_for_status()
+        return response.text
+    except(requests.HTTPError, requests.ReadTimeout):
         return None
 
 
@@ -35,26 +36,19 @@ def scrape_next_page_link(html_content):
 
 # rprint(scrape_next_page_link(fetch("https://blog.betrybe.com/")))
 
-def my_fetch():
-    response = requests.get("https://blog.betrybe.com/", headers={ "user-agent": "Fake user-agent" })
-    if response.status_code == 200:
-        selector = Selector(text=response.text)
-        article = selector.css("article.entry-preview").get()
-        # return response.headers["Content-Type"]
-        return article
-    return None
-
 
 # Requisito 4
 def scrape_noticia(html_content):
     selector = Selector(html_content)
     url = selector.css("link[rel*=canonical]::attr(href)").get()
-    title = selector.css(".entry-title::text").get().replace(u'\xa0', u'').strip()
+    title = selector.css(".entry-title::text").get()\
+        .replace(u'\xa0', u'').strip()
     date = selector.css(".meta-date::text").get()
     writer = selector.css("a.url::text").get()
     comments_count = len(selector.css(".comment-list li").getall())
     summary_html = selector.css(".entry-content p").get()
-    sopinha_de_letrinhas = BeautifulSoup(summary_html, features="lxml").get_text().replace(u'\xa0', u'').strip()
+    sopinha_de_letrinhas = BeautifulSoup(summary_html, features="lxml")\
+        .get_text().replace(u'\xa0', u'').strip()
     summary = sopinha_de_letrinhas
     tags = selector.css("section.post-tags a::text").getall()
     category = selector.css("span.label::text").get()
