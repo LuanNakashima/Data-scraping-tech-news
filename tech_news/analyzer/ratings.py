66049e9cb4376bacd2d2c3108d6a8e92924c5bc4
LuanMigNak <luan.nakashima@gmail.com>
2022-11-11 16:01:17 -0300

    req 10 e 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,34 @@
+from tech_news.database import find_news
+# from rich import print as rprint
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    db = find_news()
+    newlist = sorted(db, key=lambda d: d['comments_count'], reverse=True)
+    top_5 = []
+    for news in newlist:
+        top_5.append((news["title"], news["url"]))
+    return top_5[:5]
+
+
+# top_5_news()
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    db = find_news()
+    db_category = [news["category"] for news in db]
+    category_count = {news: db_category.count(news) for news in db_category}
+    sorted_categories = sorted(
+        sorted(list(category_count.items())), key=lambda category: (category[1]), reverse=True
+    )
+    top_5 =  [
+        category
+        for category, qnt in sorted_categories
+    ]
+
+    return top_5
+
+
+top_5_categories()
