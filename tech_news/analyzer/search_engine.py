1518df1403aba0e96cd690eca4cf6b7ba1901d3a
LuanMigNak <luan.nakashima@gmail.com>
2022-11-11 12:16:39 -0300

    req 6 e 7

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,12 +1,36 @@
+from tech_news.database import search_news
+from datetime import datetime
+# from rich import print as rprint
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    db = search_news(query)
+    news_title = []
+    for news in db:
+        news_title.append((news["title"], news["url"]))
+    return(news_title)
+
+
+# rprint(search_by_title("Notícia bacana"))
 
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        new_date = datetime.strptime(date, "%Y-%m-%d").strftime('%d/%m/%Y')
+        query = {"timestamp": new_date}
+        db = search_news(query)
+        news_date = []
+        for news in db:
+            news_date.append((news["title"], news["url"]))
+        return(news_date)
+    except ValueError:
+        raise ValueError("Data inválida")
 
+
+# rprint(search_by_date("2021/04/04"))
 
 # Requisito 8
 def search_by_tag(tag):
